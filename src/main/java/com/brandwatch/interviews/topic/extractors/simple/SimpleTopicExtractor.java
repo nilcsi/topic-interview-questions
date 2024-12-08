fd928b9363141bee330188d3ab41ffcafaae3b31
nilcsi <nilcsi@gmail.com>
2024-12-08 11:45:06 +0100

    (refactor): filter out stop words

--- src/main/java/com/brandwatch/interviews/topic/extractors/simple/SimpleTopicExtractor.java
+++ src/main/java/com/brandwatch/interviews/topic/extractors/simple/SimpleTopicExtractor.java
@@ -1,21 +1,27 @@
 package com.brandwatch.interviews.topic.extractors.simple;
 
-import org.springframework.stereotype.Component;
-
 import com.brandwatch.interviews.topic.extractors.Topic;
 import com.brandwatch.interviews.topic.extractors.TopicExtractor;
 import com.brandwatch.interviews.topic.extractors.TopicResults;
+import org.springframework.stereotype.Component;
+
+import static com.brandwatch.interviews.topic.util.StopWordsUtil.isStopWord;
 
 @Component
 public class SimpleTopicExtractor implements TopicExtractor {
     public TopicResults extract(String inputText) {
         TopicResults results = new TopicResults();
+
         String[] words = inputText.split(" ");
+
         for (String word : words) {
-            Topic topic = new Topic();
-            topic.setLabel(word);
-            results.addTopic(topic);
+            if (!isStopWord(word)) {
+                Topic topic = new Topic();
+                topic.setLabel(word);
+                results.addTopic(topic);
+            }
         }
+
         return results;
     }
 }
