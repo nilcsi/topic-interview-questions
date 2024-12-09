3b0d5df580a0a36fd22e21b758ea6a92a1bcfc91
nilcsi <nilcsi@gmail.com>
2024-12-09 19:30:44 +0100

    (feat): add logging and enhance normalizeText method

--- src/main/java/com/brandwatch/interviews/topic/extractors/simple/SimpleTopicExtractor.java
+++ src/main/java/com/brandwatch/interviews/topic/extractors/simple/SimpleTopicExtractor.java
@@ -3,6 +3,7 @@ package com.brandwatch.interviews.topic.extractors.simple;
 import com.brandwatch.interviews.topic.extractors.Topic;
 import com.brandwatch.interviews.topic.extractors.TopicExtractor;
 import com.brandwatch.interviews.topic.extractors.TopicResults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -10,13 +11,20 @@ import java.util.Arrays;
 import static com.brandwatch.interviews.topic.util.StopWordsUtil.isStopWord;
 
 @Component
+@Slf4j
 public class SimpleTopicExtractor implements TopicExtractor {
+
+    private static final String CLEAN_TEXT_REGEX = "[^a-zA-Z\\s]";
+    private static final String POSSESSIVE_SUFFIX_REGEX = "\\b(\\w+)'s\\b";
+    private static final String SPECIFIC_CHARACTERS_REGEX = "[!@#$%^&*()_+={}\\[\\]:;\"'<>,.?/|\\\\-]";
+
     public TopicResults extract(String inputText) {
         validateText(inputText);
 
         TopicResults results = new TopicResults();
 
         String cleanedText = normalizeText(inputText);
+        log.info("Normalized text: {}", cleanedText);
 
         Arrays.stream(
                 cleanedText.split("\\s+")
@@ -25,6 +33,7 @@ public class SimpleTopicExtractor implements TopicExtractor {
                 .map(this::createTopic)
                 .forEach(results::addTopic);
 
+        log.info("Successfully extracted {} topics.", results.getTopics().size());
         return results;
     }
 
@@ -34,10 +43,11 @@ public class SimpleTopicExtractor implements TopicExtractor {
         }
     }
 
-    private String normalizeText(String inputText) {
-        return inputText
-                .toLowerCase()
-                .replaceAll("[^a-zA-Z\\s]", "");
+    private String normalizeText(String text) {
+        return text.toLowerCase()
+                .replaceAll(POSSESSIVE_SUFFIX_REGEX, "$1")
+                .replaceAll(CLEAN_TEXT_REGEX, "")
+                .replaceAll(SPECIFIC_CHARACTERS_REGEX, "");
     }
 
     private Topic createTopic(String word) {
